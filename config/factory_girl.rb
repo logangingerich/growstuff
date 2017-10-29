d7a5960a47101f4d6ca0fe4fcfdbb4db8f88ac47
Logan Gingerich <loganpgingerich@gmail.com>
2017-10-29 09:09:45 -0600

    FactoryGirl Changed to FactoryBot

--- config/factory_girl.rb
+++ config/factory_girl.rb
@@ -2,8 +2,8 @@ ActionDispatch::Callbacks.after do
   # Reload the factories
   return unless Rails.env.development? || Rails.env.test?
 
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
+  unless FactoryBot.factories.blank? # first init will load factories, this should only run on subsequent reloads
+    FactoryBot.factories.clear
+    FactoryBot.find_definitions
   end
 end
